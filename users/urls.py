d4e390df92a9b0133e1f7098d644e6e9154175e8
Rafe Stefano <stefanorafe@gmail.com>
2022-09-14 22:02:01 +0200

    finish extend user

--- users/urls.py
+++ users/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import home, user_logout, register
+from .views import home, user_logout, register, user_login
 
 from django.conf.urls.static import static
 from django.conf import settings
@@ -7,5 +7,6 @@ from django.conf import settings
 urlpatterns = [
     path('', home, name='home'),
     path('logout/', user_logout, name='user_logout'),
+    path('login/', user_login, name='user_login'),
     path('register/', register, name='register'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
