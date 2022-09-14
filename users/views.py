f74c2d56f5b98f45b8f6b7a3c12876818eed2b84
Rafe Stefano <stefanorafe@gmail.com>
2022-09-14 21:39:23 +0200

    added login view

--- users/views.py
+++ users/views.py
@@ -6,6 +6,8 @@ from django.contrib import messages
 from .forms import UserForm, UserProfileForm
 from django.contrib.auth import login
 
+from django.contrib.auth.forms import AuthenticationForm
+
 
 # Create your views here.
 def home(request):
@@ -44,3 +46,22 @@ def register(request):
     }
 
     return render(request, 'users/register.html', context)
+
+
+def user_login(request):
+    form = AuthenticationForm(request, data=request.POST)
+
+    if form.is_valid():
+
+        # username = form.cleaned_data.get('username')
+        # password = form.cleaned_data.get('password')
+        # user = authenticate(username=username, password=password)
+
+        user = form.get_user()
+
+        if user:
+            login(request, user)
+            messages.success(request, 'Logged in!!')
+            return redirect('home')
+
+    return render(request, 'users/user_login.html', {'form': form})
